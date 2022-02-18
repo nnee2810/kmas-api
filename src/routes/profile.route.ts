07f1c1ebecc1e64f039097a08a76a2eaf8de4485
Namee2810 <dvn281002@gmail.com>
2022-02-18 21:23:09 +0700

    remove profile routes

--- src/routes/profile.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { profileGet } from "../controllers/profile.controller"
-
-const profileRoute = Router()
-
-profileRoute.get("/", profileGet)
-
-export default profileRoute
