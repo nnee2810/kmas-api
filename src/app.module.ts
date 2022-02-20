9fb9fbc042f001336e03a59e237e6835d985b94c
Namee2810 <dvn281002@gmail.com>
2022-02-20 20:25:14 +0700

    add schedule module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
-import { ScheduleModule } from './schedule/schedule.module';
+import { ScheduleModule } from "./modules/schedule/schedule.module"
 
 @Module({
   imports: [ScheduleModule],
