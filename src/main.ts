005e88fe380e7979692b90f4d4c03e150cb25a0d
Namee2810 <dvn281002@gmail.com>
2022-04-09 14:06:56 +0700

    update ssl

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from "@nestjs/common"
+import { HttpsOptions } from "@nestjs/common/interfaces/external/https-options.interface"
 import { NestFactory } from "@nestjs/core"
 import "dotenv/config"
+import * as fs from "fs"
 import { AppModule } from "./app.module"
 
+const httpsOptions: HttpsOptions = {
+  cert: fs.readFileSync("./ssl/certificate.crt"),
+  key: fs.readFileSync("./ssl/private.key"),
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { httpsOptions })
 
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
