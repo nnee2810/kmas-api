ed664fb88d520b3816e3fa75ed06a878049a5f7a
Namee2810 <dvn281002@gmail.com>
2022-04-09 12:07:58 +0700

    remove httpsOptions

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import { ValidationPipe } from "@nestjs/common"
-import { HttpsOptions } from "@nestjs/common/interfaces/external/https-options.interface"
 import { NestFactory } from "@nestjs/core"
 import "dotenv/config"
-import * as fs from "fs"
 import { AppModule } from "./app.module"
-
-const httpsOptions: HttpsOptions = {
-  cert: fs.readFileSync("./ssl/certificate.crt"),
-  key: fs.readFileSync("./ssl/private.key"),
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
