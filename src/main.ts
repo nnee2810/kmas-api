2aa4a0224b298349382750d30a2bd649ef19f5eb
Namee2810 <dvn281002@gmail.com>
2022-08-01 23:26:27 +0700

    improve: remove ssl

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { ValidationPipe } from "@nestjs/common"
-import { HttpsOptions } from "@nestjs/common/interfaces/external/https-options.interface"
 import { NestFactory } from "@nestjs/core"
 import "dotenv/config"
-import * as fs from "fs"
 import { AppModule } from "./app.module"
 import { TransformInterceptor } from "./interceptors/transform.interceptor"
 
-const httpsOptions: HttpsOptions = {
-  cert: fs.readFileSync("./ssl/certificate.crt"),
-  key: fs.readFileSync("./ssl/private.key"),
-}
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { httpsOptions })
+  const app = await NestFactory.create(AppModule)
 
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
