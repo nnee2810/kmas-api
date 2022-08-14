71ea7880a601d541d009adac9ff7143b54d352d4
Namee2810 <dvn281002@gmail.com>
2022-08-14 23:14:11 +0700

    feature: add ssl

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import "dotenv/config"
+import fs from "fs"
 import { AppModule } from "./app.module"
 import { TransformInterceptor } from "./interceptors/transform.interceptor"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      cert: fs.readFileSync(__dirname + "/ssl/certificate.crt"),
+      key: fs.readFileSync(__dirname + "/ssl/private.key"),
+    },
+  })
 
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
