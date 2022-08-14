b7a32bf269c4709228d9246ba330a3cfbefd073a
Namee2810 <dvn281002@gmail.com>
2022-08-15 00:28:40 +0700

    fix: revert all

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
 import "dotenv/config"
-import * as fs from "fs"
 import { AppModule } from "./app.module"
 import { TransformInterceptor } from "./interceptors/transform.interceptor"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: {
-      ca: fs.readFileSync(__dirname + "/../ssl/ca_bundle.crt"),
-      key: fs.readFileSync(__dirname + "/../ssl/private.key"),
-    },
-  })
+  const app = await NestFactory.create(AppModule)
 
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
