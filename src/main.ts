5719e3d90fbe020da7aeef84db39fdd2a6302f07
Namee2810 <dvn281002@gmail.com>
2022-08-15 08:35:52 +0700

    fix: remove nginx from docker

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new TransformInterceptor())
 
-  await app.listen(process.env.PORT)
+  await app.listen(5000)
 }
 bootstrap()
