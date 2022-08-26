369a19f1837293154a1d473fb8eac5b97b105181
Namee2810 <dvn281002@gmail.com>
2022-08-26 16:33:35 +0700

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
-import * as cookieParser from "cookie-parser"
-import * as csurf from "csurf"
 import * as fs from "fs"
 import helmet from "helmet"
 import { AppModule } from "./app.module"
@@ -12,13 +10,9 @@ async function bootstrap() {
       key: fs.readFileSync(process.env.SSL_KEY_PATH),
       cert: fs.readFileSync(process.env.SSL_CERT_PATH),
     },
-    cors: {
-      origin: ["localhost:3000", "kmas.nnee.dev"],
-    },
   })
 
-  app.use(cookieParser())
-  app.use(csurf())
+  app.enableCors()
   app.use(helmet())
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
