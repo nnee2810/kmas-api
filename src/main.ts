29da67195eb7829d466994540ab17fa3e590f74e
Namee2810 <dvn281002@gmail.com>
2022-09-11 13:39:28 +0700

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,7 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors({
-    origin: function (origin, callback) {
-      if ([process.env.APP_URL].indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error("Not allowed by CORS"))
-      }
-    },
-  })
+  app.enableCors()
   app.use(helmet())
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
