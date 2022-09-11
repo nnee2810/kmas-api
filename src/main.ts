a86f3139da08a8d208f0ff66260155ed1dc4f6b3
Namee2810 <dvn281002@gmail.com>
2022-09-11 11:36:39 +0700

    improve: circleci, env, docker

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
-import * as fs from "fs"
 import helmet from "helmet"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    httpsOptions: process.env.NODE_ENV === "production" && {
-      key: fs.readFileSync(process.env.SSL_KEY_PATH),
-      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
-    },
-  })
+  const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === "production" ? process.env.APP_URL : undefined,
+  })
   app.use(helmet())
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
