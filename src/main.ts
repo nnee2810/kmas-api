01c7e559300e96ea3fa670f29db7e1bc18a1c6d2
Namee2810 <dvn281002@gmail.com>
2022-09-11 20:24:46 +0700

    fix: add ssl

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,11 @@ import { AppModule } from "./app.module"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions: process.env.NODE_ENV === "prod" && {
-      cert: fs.readFileSync("./ssl/cert.pem"),
+      cert: fs.readFileSync("../ssl/cert.pem"),
       key: fs.readFileSync("../ssl/private.pem"),
     },
   })
 
-  app.enableCors()
   app.use(helmet())
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
 
